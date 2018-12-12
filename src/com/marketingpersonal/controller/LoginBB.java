72cccf5ae5254b1270003af3f99ab46c25497a50
Jarrison Garcia <garciajarrison@gmail.com>
2018-12-12 07:30:10 -0500

    asd
    asd

--- src/com/marketingpersonal/controller/LoginBB.java
+++ src/com/marketingpersonal/controller/LoginBB.java
@@ -78,7 +78,7 @@ public class LoginBB extends SpringBeanAutowiringSupport implements Serializable
 		ldap = new LoginLDAP();
 
 		try {
-			if(validar() /*&& ldap.login(usuario.getUsuario(), usuario.getContrasena())*/) {	
+			if(validar() && ldap.login(usuario.getUsuario(), usuario.getContrasena())) {	
 				
 				usuario = this.getUsuarioService().login(usuario);
 				if(usuario != null) {
